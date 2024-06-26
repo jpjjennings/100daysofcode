89a71cca1001fe968015129dce376e29b9d4c7a9
Jimmy Jennings <jp.jennings@aol.com>
2024-06-26 18:01:49 +0100

    Create day1-project-bandnamegenerator.py

---
+++ day1/day1-project-bandnamegenerator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator!\n")
+city = input("What is the name of the city that you grew up in?\n")
+pet = input("What was the name of your first pet?\n")
+band_name = city + " " + pet
+print("\nRad! Your new band name could be " + band_name)
