4b7e2ae1aeca62e9114a3cd1b4480a4bac8d366d
Jimmy Jennings <jp.jennings@aol.com>
2024-06-26 14:30:28 +0100

    Create day1-variables.py

---
+++ day1-variables.py
@@ -0,0 +1,16 @@
+# There are two variables, a and b from input
+a = input()
+b = input()
+# 🚨 Don't change the code above ☝️
+####################################
+# Write your code below this line 👇
+
+# The below will switch the values of variables 'a' and 'b'
+c = a
+a = b
+b = c
+
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
