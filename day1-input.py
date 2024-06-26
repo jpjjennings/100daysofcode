37d79ac647e706e6bc76694abd4a26edc24a20e0
Jimmy Jennings <jp.jennings@aol.com>
2024-06-26 14:28:13 +0100

    Create day1-input.py

---
+++ day1-input.py
@@ -0,0 +1,7 @@
+num1 = int(input())
+num2 = int(input())
+
+print(num1 * num2)
+
+numOfLetters = len(input("Please write a word, and by magic I will tell you how many letters long it is!\n"))
+print(numOfLetters)
