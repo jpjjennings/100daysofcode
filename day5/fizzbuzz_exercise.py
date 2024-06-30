3a7c98afea9684cc90b7ec8225769fa787d67113
Jimmy Jennings <jp.jennings@aol.com>
2024-06-30 10:18:45 +0100

    Create fizzbuzz_exercise.py

---
+++ day5/fizzbuzz_exercise.py
@@ -0,0 +1,23 @@
+# Write your code here 👇
+for number in range(1, 101):
+  if (number % 3) < 1 and (number % 5) <1:
+    print("FizzBuzz")
+  elif (number % 3) < 1:
+    print("Fizz")
+  elif (number % 5) < 1:
+    print("Buzz")
+  else:
+    print(number)
+
+###Solution
+##target = 100
+##for number in range(1, target + 1):
+##  if (number % 3) < 1 and (number % 5) <1:
+ ##   print("FizzBuzz")
+##  elif (number % 3) < 1:
+ ##   print("Fizz")
+ ## elif (number % 5) < 1:
+##    print("Buzz")
+##  else:
+    ##print(number)
+    
